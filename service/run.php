5f4e312bd4a86df348fe2d2342d8dde981b7e39c
Josh DeWitt <jdewitt@unomaha.edu>
2014-04-25 12:00:12 -0500

    Now show raw g-code success again.

--- service/run.php
+++ service/run.php
@@ -24,7 +24,7 @@
         writeToPipe( getGcodePipePath(), 'G91' . $command, true );
     } elseif ( array_key_exists( 'rawGcode', $_POST ) ) {
         $gCode = $_POST['rawGcode'] . "\n";
-        writeToPipe( getGcodePipePath(), $gCode, false );
+        writeToPipe( getGcodePipePath(), $gCode, true );
     } elseif ( array_key_exists( 'setUserHome', $_POST ) ) {
         writeToPipe( getGcodePipePath(), 'G100', true );
     } elseif ( array_key_exists( 'homeMachine', $_POST ) ) {
