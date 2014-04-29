3f76682a67652e892a76d2d0745b8120f526d1bc
Josh DeWitt <jdewitt@unomaha.edu>
2014-04-29 10:24:31 -0500

    polished leap interface

--- js/leap.js
+++ js/leap.js
@@ -9,10 +9,11 @@ var deltaX = 0;
 var deltaY = 0;
 var deltaZ = 0;
 var hasMoved = 0;
-var hand = true;
+var hand = 1;
+var timeout = 110;
+var spindleState = 0;
 
-var controller = new Leap.Controller();
-
+var controller = new Leap.Controller({enableGestures: true});
 function setLeapToZero() {
     macX = 0;
     macY = 0;
@@ -25,96 +26,143 @@ function moveToZero() {
     curZ = 0;
 }
 
+function moveToLeapHome() {
+    curX = 0;
+    curY = 0;
+    curZ = 10;
+}
+
 function abs( i ) {
     if( i >= 0 )
         return  i;
     else
         return -i;
 }
+// wish i had math...
+function scale( i ) {
+    j = 0;
+    t = 0;
+    if( i > 0 ){
+        j = 0;
+        t = 110;
+    }
+    if( i > 4 ){
+        j = 2;
+        t = 170;
+    }
+    if( i > 9 ){
+        j = 3;
+        t = 230;
+    }
+    if( i > 16 ){
+        j = 4;
+        t = 290;
+    }
+    if( i > 25 ){
+        j = 5;
+        t = 350;
+    }
+    if( timeout < t )
+        timeout = t;
+    return j;
+}
 
 function sign( i ) {
+
     if( i >= 0 )
-        return 1;
+        return scale(i);
     else
-        return -1;
+        return -scale(abs(i));
+}
+
+function checkBounds() {
+    if( macY > 80 )
+        macY = 80;
+    if( macY < -80 )
+        macY = -80;
+    if( macX > 80 )
+        macX = 80;
+    if( macX < -80 )
+        macX = -80;
+    if( macZ > 55 )
+        macZ = 55;
+    if( macZ < 0 )
+        macZ = 0;  
 }
 
 function moveHeadByLeap() {
     deltaX = curX - macX;
-    if( abs( deltaX ) > 5 ){
-        hasMoved |= 1;
+    if( abs( deltaX ) > 5*hand ){
+        hasMoved++;
         macX = macX + (sign(deltaX));
     }
     deltaY = curY - macY;
-    if( abs( deltaY ) > 5 ){
-        hasMoved |= 2;
+    if( abs( deltaY ) > 5*hand ){
+        hasMoved++;
         macY = macY + (sign(deltaY));
     }
     deltaZ = curZ - macZ;
-    if( abs( deltaZ ) > 5 ){
-        hasMoved |= 4;
+    if( abs( deltaZ ) > 5*hand ){
+        hasMoved++;
         macZ = macZ + (sign(deltaZ));
     }
-
-    if( !( hasMoved & 1 ) && deltaX && curX == 0 ) {
-        hasMoved |= 1;
-        macX = 0;
-    }
-    if( !( hasMoved & 2 ) && deltaY && curY == 0 ) {
-        hasMoved |= 2;
-        macY = 0;
-    }
-    if( !( hasMoved & 4 ) && deltaZ && curZ == 0 ) {
-        hasMoved |= 4;
+    if( curZ < 5 ){
+        sign( macZ );// just to get proper timeout
         macZ = 0;
     }
-
+    checkBounds();
     if( hasMoved ){
         hasMoved = 0;
         $.post( '/service/run.php', {'rawGcode': 'G90X'.concat( parseInt( ( macX ) ) ).concat( 'Y' ).concat( parseInt( ( macY ) ) ).concat( 'Z' ).concat( parseInt( ( macZ ) ) )} );
     }
 }
 
-this.controller.on( 'connect', function() {
-        setInterval( function() {
-            var obj = controller.frame();
-            if( obj.hands.length > 0 && $( '#leapCheck' ).is( ':checked' ) ) {
-                // only run this at state change...
-                if( !hand ) {
-                    hand = true;
-                    $.post( '/service/run.php', {'rawGcode': 'M03\nG101|  Leap Control'} );
-                }
-                curY = -parseInt(obj.hands[0].palmPosition[0]/2);
-                curZ = parseInt((obj.hands[0].palmPosition[1]-200)/5);
-                curX = -parseInt(obj.hands[0].palmPosition[2]/2);
-
-                // SET BOUNDS
-                if( curY > 80 )
-                    curY = 80;
-                if( curY < -80 )
-                    curY = -80;
-                if( curX > 80 )
-                    curX = 80;
-                if( curX < -80 )
-                    curX = -80;
-                if( curZ > 50 )
-                    curZ = 50;
-                if( curZ < 0 )
-                    curZ = 0;
-
-                moveHeadByLeap();
+function processCommand() {
+    var obj = controller.frame();
+    if( obj.gestures.length > 0 ){
+        var gesture = obj.gestures[0];
+        if( gesture.type == "circle" && gesture.progress >= 2 ){
+            if( gesture.normal[2]<=0){
+                $.post( '/service/run.php', {'rawGcode': 'M03'} );
             } else {
-                if( hand ) {
-                    hand = false;
-                    $.post( '/service/run.php', {'rawGcode': 'M05\nC101|Waiting for user'} );
-                    moveToZero();
-                }
+                $.post( '/service/run.php', {'rawGcode': 'M05'} );
+             }
+        }
+        //else if( gesture.type == "keyTap" )
+        //    $.post( '/service/run.php', {'rawGcode': 'Z0\nG91X2Y2\nG91Z2X-2\nG90Z0\nG91X2Y-2\nG91X-2\n'} );
+    }
+    
+    if( obj.hands.length > 0 && $( '#leapCheck' ).is( ':checked' ) ) {
+        // only run this at state change...
+        if( !hand ) {
+            hand = 1;
+            spindleState = 1;
+            $.post( '/service/run.php', {'rawGcode': 'G101|  Leap Control'} );
+        }
 
-                if( macX || macY || macZ ) {
-                    moveHeadByLeap();
-                }
-            }
-        }, 100);
+        curY = -parseInt(obj.hands[0].palmPosition[0]/2);
+        curZ = parseInt((obj.hands[0].palmPosition[1]-200)/5);
+        curX = -parseInt(obj.hands[0].palmPosition[2]/2);
+        moveHeadByLeap();
+    } else {
+        if( hand ) {
+            hand = 0;
+            spindleState = 0;
+            $.post( '/service/run.php', {'rawGcode': 'M05\nC101|Waiting for user'} );
+            moveToLeapHome();
+        }
+
+        if( macX || macY || macZ ) {
+            moveHeadByLeap();
+        }
+    }
+    setTimeout( function() { processCommand() }, timeout );
+    timeout = 0;
+}
+
+this.controller.on( 'connect', function() {
+    processCommand();
+        //setInterval( function() { processCommand() }, timeout);
 });
 
 controller.connect();
